5b77b3273cf2a7cc1ee96f3fca9bc879a46940c0
Paul <paul.bratslavsky@gmail.com>
2019-08-20 16:00:38 -0500

    basic layout complete next to start workin on mobile view

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 console.log('connected...');
+
+
+
+function initMap() {
+  // your location 
+  const local = { lat: 42.361145, lng: -71.057083 };
+
+  // center map on location
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: local
+  });
+
+  // the marker position
+  const marker = new google.maps.Marker({ position: local,
+  map: map });
+}
+
+console.log(map);
